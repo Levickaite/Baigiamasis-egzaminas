bb22e7d3799135c0e5761adc419939520002b013
Indre <levickaite@yahoo.com>
2025-10-15 19:31:14 +0300

    React install

---
+++ Backend/server.js
@@ -0,0 +1,39 @@
+import dotenv from 'dotenv'
+import express from 'express'
+import pratimaiRoutes from './routes/pratimai.js'
+import mongoose from 'mongoose'
+import userRoutes from './routes/user.js'
+
+dotenv.config()
+
+//express app
+const app =express()
+//middleware
+app.use(express.json())
+app.use((req, res, next)=> {
+    console.log(req.path, req.method)
+    next()
+    
+})
+//routes 
+
+// app.use('/api/pratimai', pratimaiRoutes)
+// app.use('/api/user', userRoutes)
+app.get('/',(req, res)=>{
+    res.json({mssg: 'Welcome to the app!'})
+} )
+
+//connect to DB
+mongoose.connect(process.env.URI)
+    .then(()=>{
+        app.listen(process.env.PORT, ()=>{
+            console.log('listening on port', process.env.PORT);
+            
+        })
+    })
+    .catch((err)=> console.log(err)
+    )
+
+
+
+
